93d6827ab9a28b9c7a184523dd416d0ee8fde19e
tyousofine <tyousofine@gmail.com>
2024-02-15 18:36:38 -0500

    render

--- client/src/components/RecipeRequest/index.js
+++ client/src/components/RecipeRequest/index.js
@@ -19,7 +19,7 @@ export default function RecipeRequest({ ingredients }) {
 
     // gp3 API POST request function
     const gp3RequestHandler = async () => {
-        const response = await axios.post('http://localhost:3000/api/recipe', {
+        const response = await axios.post('https://pantrypal-server-plc3.onrender.com/api/recipe', {
             Method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -47,7 +47,7 @@ export default function RecipeRequest({ ingredients }) {
 
     // dalle-3 POST request function
     const dalleRequestHandler = async (dataForDalle) => {
-        const response = await axios.post('http://localhost:3000/api/image', {
+        const response = await axios.post('https://pantrypal-server-plc3.onrender.com/api/image', {
             Method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
