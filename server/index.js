d8fd9097b756ac29351e488b0f0148b71e0fa022
tyousofine <tyousofine@gmail.com>
2024-03-07 20:36:32 -0500

    -

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ app.use(cors());
 // app.use((req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", "*")
 // })
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 dotenv.config();
 const OpenAI = require('openai');
 
