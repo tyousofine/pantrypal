c0f16d4e414f18a94dea902f0d1de1103b839b4e
tyousofine <tyousofine@gmail.com>
2024-03-27 11:35:26 -0400

    add toggle button for form

--- client/src/pages/InputPage/index.js
+++ client/src/pages/InputPage/index.js
@@ -8,16 +8,31 @@ import RecipeRequest from '../../components/RecipeRequest'
 
 export default function InputPage() {
     const [formData, setFormData] = useState('');
-    console.log({ formData })
+    const [formOn, setFormOn] = useState(true);
+    const [formButton, setFormButton] = useState(false);
+
 
     const handleData = (data) => {
         setFormData(data);
+        setFormOn(false);
+        setFormButton(true);
     };
+
+    const formAndButtonToggleHandler = (e) => {
+        e.preventDefault();
+        setFormOn(true);
+        setFormButton(false);
+    }
+
+
 
     return (
         <>  <NavBar />
-
-            <IngredientsForm onDataSubmit={handleData} />
+            {formOn &&
+                <IngredientsForm onDataSubmit={handleData} />
+            }
+            {!!formButton &&
+                <button onClick={formAndButtonToggleHandler}>test</button>}
             <RecipeRequest ingredients={formData} />
         </>
     )
